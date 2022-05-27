f084190195b2e9a802ff81135306ca4165ed30d7
xyxdbp2022 <xyxdbp2022@gmail.com>
2022-05-28 01:21:48 +0800

    add alacritty config

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,6 +1,15 @@
 export PATH=$PATH:/opt/homebrew/bin
+export PATH=$PATH:/usr/local/bin
 EDITOR=nvim
 export EDITOR
 
 export JAVA_HOME="/Users/wuguipeng/Environments.localized/zulu8/zulu-8.jdk/Contents/Home"
 export PATH=$JAVA_HOME/bin:$PATH
+
+
+export GRADLE_HOME="/Users/wuguipeng/Environments.localized/gradle-6.8.3"
+export PATH=$GRADLE_HOME/bin:$PATH
+
+
+export MAVEN_HOME="/Users/wuguipeng/Environments.localized/apache-maven-3.8.5"
+export PATH=$MAVEN_HOME/bin:$PATH
