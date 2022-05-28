ffa8ac2279409f049558ec4e336f78b0fe652c58
xyxdbp2022 <xyxdbp2022@gmail.com>
2022-05-28 13:02:24 +0800

    add tmux config

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,15 +1,20 @@
-export PATH=$PATH:/opt/homebrew/bin
-export PATH=$PATH:/usr/local/bin
 EDITOR=nvim
 export EDITOR
 
-export JAVA_HOME="/Users/wuguipeng/Environments.localized/zulu8/zulu-8.jdk/Contents/Home"
-export PATH=$JAVA_HOME/bin:$PATH
+NAME=uname
+# mac
+if test $NAME = "Darwin"; then
+	export PATH=$PATH:/opt/homebrew/bin
+	export PATH=$PATH:/usr/local/bin
 
+	export JAVA_HOME="/Users/wuguipeng/Environments.localized/zulu8/zulu-8.jdk/Contents/Home"
+	export PATH=$JAVA_HOME/bin:$PATH
 
-export GRADLE_HOME="/Users/wuguipeng/Environments.localized/gradle-6.8.3"
-export PATH=$GRADLE_HOME/bin:$PATH
+	export GRADLE_HOME="/Users/wuguipeng/Environments.localized/gradle-6.8.3"
+	export PATH=$GRADLE_HOME/bin:$PATH
 
+	export MAVEN_HOME="/Users/wuguipeng/Environments.localized/apache-maven-3.8.5"
+	export PATH=$MAVEN_HOME/bin:$PATH
+else # linux
+fi
 
-export MAVEN_HOME="/Users/wuguipeng/Environments.localized/apache-maven-3.8.5"
-export PATH=$MAVEN_HOME/bin:$PATH
